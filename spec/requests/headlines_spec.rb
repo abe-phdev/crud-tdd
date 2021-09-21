a360f476850c5391da0f8a99ca865da7df514bb8
Raymond <raymond@philippinedev.com>
2021-09-21 17:59:12 +0800

    added test for index and create method on controller

--- spec/requests/headlines_spec.rb
+++ spec/requests/headlines_spec.rb
@@ -2,6 +2,46 @@ require 'rails_helper'
 
 RSpec.describe "Headlines", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    context 'when the user is not authenticated' do
+      it 'will redirect to login' do
+        get headlines_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when the user is authenticated' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in(user)
+      end
+
+      it 'will show the list of headlines' do
+        get headlines_path
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "GET /create" do
+    context 'when the user is not authenticated' do
+      it 'will redirect to log in path' do
+        get headlines_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when the user is authenticated' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in(user)
+      end
+
+      it 'will let the user to create' do
+        get headlines_path
+        expect(response).to be_successful
+      end
+    end
   end
 end
