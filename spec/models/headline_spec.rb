f6422dde9498c65c2d6ae6cd60b35f8bd37d3447
Raymond <raymond@philippinedev.com>
2021-09-21 17:58:11 +0800

    added test for index and create

--- spec/models/headline_spec.rb
+++ spec/models/headline_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Headline, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:title) { Faker::Book.title }
+  let(:content) { Faker::Lorem.paragraphs }
+  let(:headline) { FactoryBot.build(:headline, title: title, content: content) }
+
+  describe 'relationship' do
+    it { expect(headline).to belong_to(:user) }  
+  end
+
+  describe '#title' do
+    context 'when empty' do
+      let(:title) { '' }
+
+      it 'will error' do
+        expect(headline).to_not be_valid
+      end
+    end
+
+    context 'when valid' do
+      it 'will succeed' do
+        expect(headline).to be_valid
+      end
+    end
+  end
+
+  describe '#content' do
+    context 'when empty' do
+      let(:content) { '' }
+
+      it 'will error' do
+        expect(headline).to_not be_valid
+      end
+    end
+
+    context 'when valid' do
+      it 'will succeed' do
+        expect(headline).to be_valid
+      end
+    end
+  end
 end
